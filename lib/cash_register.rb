967f2cdb0efac9c4c40995a55acb8585780c2e83
Sergio Robles <sroblesc@gmail.com>
2020-01-08 16:35:58 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
-
+class CashRegister
+  
+  attr_accessor :total
+  
+  def initialize
+    @total = 0 
+    
+  end
+  
+  def total 
+    returns total 
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+end
